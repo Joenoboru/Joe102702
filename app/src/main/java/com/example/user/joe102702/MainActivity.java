5b09a30fbcb16b3903eef94b796a4261a8f20afc
joenoboru <joe4580803@gmail.com>
2016-10-27 13:49:06 +0800

    index 跳至第二頁 ,並設定按手機返回鍵回至第一頁

--- app/src/main/java/com/example/user/joe102702/MainActivity.java
+++ app/src/main/java/com/example/user/joe102702/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.joe102702;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
@@ -11,10 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StrictMode.VmPolicy policy = new StrictMode.VmPolicy.Builder()
+                .detectFileUriExposure()
+                .build();
+        StrictMode.setVmPolicy(policy);
+
         wv = (WebView)findViewById(R.id.webView);
         wv.setWebChromeClient(new WebChromeClient());
         wv.getSettings().setJavaScriptEnabled(true);
         //wv.loadUrl("https://tw.mobi.yahoo.com");用web view打開顯示 url
         wv.loadUrl("file:///android_asset/index.html");//用web view顯示assets內的html內容
+    }
+    @Override
+    public void onBackPressed() {
+        if (wv.getUrl().contains("index.html"))
+        {
+            super.onBackPressed();
+        }
+        else
+        {
+            wv.goBack();
+        }
+
     }
 }
